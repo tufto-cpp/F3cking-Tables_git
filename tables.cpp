cd8b22131f8cc56b7c12fd77aca7efa00a49a6bf
tufto-cpp <yastalexei@gmail.com>
2023-11-11 15:22:02 +0600

    puting circles

--- tables.cpp
+++ tables.cpp
@@ -74,16 +74,97 @@ Vector2 GetDirection(Vector2 input){
 	return input;
 }
 
-Vector2 GetForceVector(Vector2 pos1, Vector2 pos2, float charge1, float charge2){ // pos1 is current processing table
-	Vector2 direction;															 // pos2 is an object, which influence we
-	direction.x = pos1.x - pos2.x;												 // are calculating
+Vector2 GetForceVector(Vector2 pos1/*current table*/, Vector2 pos2/*from where force is*/, float charge1, float charge2){ // pos1 is current processing table
+	Vector2 direction;												// pos2 is an object, which influence we
+	direction.x = pos1.x - pos2.x;									// are calculating
 	direction.y = pos1.y - pos2.y;
 	float distance = GetLength(direction);
 	direction = GetDirection(direction);
 	float force = charge1*charge2/(distance*distance);
 	Vector2 forceVector;
-	forceVector.x = direction.x*force/10; 
-	forceVector.y = direction.y*force/10;
+	forceVector.x = direction.x*force; 
+	forceVector.y = direction.y*force;
+	return forceVector;
+}
+
+Vector2 GetForceFromWalls(table Table, std::vector<wall>& walls){
+	Vector2 upper,lower,left,right;
+	float miny = 450,maxy = 0,minx = 800,maxx = 0;
+	for(int i=0;i<walls.size();i++){
+		if((Table.position.x >= walls[i].first.x && Table.position.x <= walls[i].second.x)||(Table.position.x <= walls[i].first.x && Table.position.x >= walls[i].second.x)){
+			float Yw,Yv,Xw,Xv;
+			if(walls[i].first.y >= walls[i].second.y){
+				Yw = walls[i].first.y;
+				Yv = walls[i].second.y;
+				Xw = walls[i].first.x;
+				Xv = walls[i].second.x;
+			} else {
+				Yv = walls[i].first.y;
+				Yw = walls[i].second.y;
+				Xv = walls[i].first.x;
+				Xw = walls[i].second.x;
+			}
+			float X = Table.position.x,Y = Table.position.y;
+			float XwXv = fabs(Xw - Xv);
+			float YwYv = Yw - Yv;
+			float XvX = fabs(Xv - X);
+			float YvY0 = (YwYv*XvX)/XwXv;
+			float Y0 = Yv+YvY0;
+			if(Y0 > Y){
+				if(Y0 < miny){
+					miny = Y0;
+				}
+			} else {
+				if(Y0 > maxy){
+					maxy = Y0;
+				}
+			}
+		} 
+		if((Table.position.y >= walls[i].first.y && Table.position.y <= walls[i].second.y)||(Table.position.y <= walls[i].first.y && Table.position.y >= walls[i].second.y)){
+			float Yw,Yv,Xw,Xv;
+			if(walls[i].first.x >= walls[i].second.x){
+				Yw = walls[i].first.y;
+				Yv = walls[i].second.y;
+				Xw = walls[i].first.x;
+				Xv = walls[i].second.x;
+			} else {
+				Yv = walls[i].first.y;
+				Yw = walls[i].second.y;
+				Xv = walls[i].first.x;
+				Xw = walls[i].second.x;
+			}
+			float X = Table.position.x,Y = Table.position.y;
+			float YwYv = fabs(Yw - Yv);
+			float XwXv = Xw - Xv;
+			float YvY = fabs(Yv - Y);
+			float XvX0 = (XwXv*YvY)/YwYv;
+			float X0 = Xv + XvX0;
+			if(X0 > X){
+				if(X0 < minx){
+					minx = X0;
+				}
+			} else {
+				if(X0 > maxx){
+					maxx = X0;
+				}
+			}
+		}
+	}
+	upper.x = Table.position.x;
+	upper.y = maxy;
+	lower.x = Table.position.x;
+	lower.y = miny;
+	left.x = maxx;
+	left.y = Table.position.y;
+	right.x = minx;
+	right.y = Table.position.y;
+	upper = GetForceVector(Table.position, upper, Table.charge, 10);
+	lower = GetForceVector(Table.position, lower, Table.charge, 10);
+	left = GetForceVector(Table.position, left, Table.charge, 10);
+	right = GetForceVector(Table.position, right, Table.charge, 10);
+	Vector2 forceVector;
+	forceVector.x = upper.x+lower.x+left.x+right.x;
+	forceVector.y = upper.y+lower.y+left.y+right.y;
 	return forceVector;
 }
 
@@ -106,7 +187,7 @@ int main(void)
     bool stable = true; // checks if the sistem is stable? if new table added its unstable
     
     int curNum = 0;
-    int tableNum = 2;
+    int tableNum = 8;
 
     SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
     //--------------------------------------------------------------------------------------
@@ -146,17 +227,7 @@ int main(void)
 			//calculating forces
 			//------------------------------------------------------------------------------
 			for(int i=0;i<tables.size();i++){
-				Vector2 forceVector;
-				forceVector.x = 0;
-				forceVector.y = 0;
-				for(int g=0;g<walls.size();g++){
-					Vector2 center;
-					center.x = (walls[g].first.x + walls[g].second.x)/2;
-					center.y = (walls[g].first.y + walls[g].second.y)/2;
-					Vector2 v = GetForceVector(tables[i].position, center, tables[i].charge, walls[g].charge);
-					forceVector.x += v.x;
-					forceVector.y += v.y;
-				}
+				Vector2 forceVector = GetForceFromWalls(tables[i], walls);
 				for(int g=0;g<tables.size();g++){
 					if(g!=i){
 						Vector2 v = GetForceVector(tables[i].position, tables[g].position, tables[i].charge, tables[g].charge);
